3a25fb16c9202744e5567baf64e027ea3e3d8b4e
aiyu-ayaan <aiyuayaan22@gmail.com>
2024-09-19 23:35:02 +0530

    feat: Add home screen for teacher
    - Added a basic home screen for teachers using `ListDetailPaneScaffold`.
    - The home screen displays a list of
     items in the list pane.
    - Clicking on an item navigates to the detail pane and displays the item's content.
    - Updated the `MainScreen` to use `NavigationRail` for expanded screens and navigate to the `HomeScreen`.

--- composeApp/src/commonMain/kotlin/com/atech/research/ui/compose/main/MainScreen.kt
+++ composeApp/src/commonMain/kotlin/com/atech/research/ui/compose/main/MainScreen.kt
@@ -1,22 +1,14 @@
 package com.atech.research.ui.compose.main
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material.icons.outlined.Dashboard
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
-import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
 import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
-import androidx.compose.material3.adaptive.layout.AnimatedPane
-import androidx.compose.material3.adaptive.layout.ListDetailPaneScaffold
-import androidx.compose.material3.adaptive.layout.ListDetailPaneScaffoldRole
-import androidx.compose.material3.adaptive.navigation.rememberListDetailPaneScaffoldNavigator
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffoldDefaults
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScope
@@ -29,11 +21,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.unit.dp
 import androidx.window.core.layout.WindowWidthSizeClass
 import com.atech.research.LocalDataStore
 import com.atech.research.core.model.UserType
-import com.atech.research.utils.BackHandler
+import com.atech.research.ui.compose.teacher.home.compose.HomeScreen
 import com.atech.research.utils.PreferenceUtils
 import com.atech.research.utils.Prefs
 
@@ -47,53 +38,6 @@ enum class TeacherAppDestinations(
 
 }
 
-@OptIn(ExperimentalMaterial3AdaptiveApi::class)
-@Composable
-fun Container(modifier: Modifier = Modifier) {
-    val navigator = rememberListDetailPaneScaffoldNavigator<Any>()
-    BackHandler(navigator.canNavigateBack()) {
-        navigator.navigateBack()
-    }
-    ListDetailPaneScaffold(
-        directive = navigator.scaffoldDirective,
-        value = navigator.scaffoldValue,
-        listPane = {
-            AnimatedPane {
-                LazyColumn(
-                    modifier = Modifier.fillMaxSize()
-                ) {
-                    items(100) { text ->
-                        Column(
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .clickable {
-                                    navigator.navigateTo(
-                                        pane = ListDetailPaneScaffoldRole.Detail,
-                                        content = text
-                                    )
-                                }
-                                .padding(16.dp),
-                            verticalArrangement = Arrangement.Center,
-                            horizontalAlignment = Alignment.CenterHorizontally
-                        ) {
-                            Text("Item $text")
-                        }
-                    }
-                }
-            }
-        },
-        detailPane = {
-            val content = navigator.currentDestination?.content?.toString() ?: "No content"
-            AnimatedPane {
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                ) {
-                    Text("Detail: $content")
-                }
-            }
-        }
-    )
-}
 
 @Composable
 fun MainScreen() {
@@ -102,7 +46,7 @@ fun MainScreen() {
     val adaptiveInfo = currentWindowAdaptiveInfo()
     val customNavSuiteType = with(adaptiveInfo) {
         if (windowSizeClass.windowWidthSizeClass == WindowWidthSizeClass.EXPANDED) {
-            NavigationSuiteType.NavigationDrawer
+            NavigationSuiteType.NavigationRail
         } else {
             NavigationSuiteScaffoldDefaults.calculateFromAdaptiveInfo(adaptiveInfo)
         }
@@ -122,7 +66,7 @@ fun MainScreen() {
         }
     ) {
         when (currentDestination) {
-            TeacherAppDestinations.Home -> Container()
+            TeacherAppDestinations.Home -> HomeScreen()
             TeacherAppDestinations.Profile -> {
                 Column(
                     modifier = Modifier.fillMaxSize(),
--- composeApp/src/commonMain/kotlin/com/atech/research/ui/compose/teacher/home/compose/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/atech/research/ui/compose/teacher/home/compose/HomeScreen.kt
@@ -1,17 +1,79 @@
 package com.atech.research.ui.compose.teacher.home.compose
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.AccountCircle
-import androidx.compose.material.icons.outlined.Dashboard
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
+import androidx.compose.material3.adaptive.layout.AnimatedPane
+import androidx.compose.material3.adaptive.layout.ListDetailPaneScaffold
+import androidx.compose.material3.adaptive.layout.ListDetailPaneScaffoldRole
+import androidx.compose.material3.adaptive.navigation.rememberListDetailPaneScaffoldNavigator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+import com.atech.research.common.MainContainer
+import com.atech.research.utils.BackHandler
 
 
-
+@OptIn(ExperimentalMaterial3AdaptiveApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     modifier: Modifier = Modifier
 ) {
-
+    val navigator = rememberListDetailPaneScaffoldNavigator<Any>()
+    BackHandler(navigator.canNavigateBack()) {
+        navigator.navigateBack()
+    }
+    MainContainer(
+        modifier = modifier,
+        title = "Home",
+        enableTopBar = true
+    ){paddingValue->
+        ListDetailPaneScaffold(
+            modifier = Modifier.padding(paddingValue),
+            directive = navigator.scaffoldDirective,
+            value = navigator.scaffoldValue,
+            listPane = {
+                AnimatedPane {
+                    LazyColumn(
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        items(100) { text ->
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .clickable {
+                                        navigator.navigateTo(
+                                            pane = ListDetailPaneScaffoldRole.Detail,
+                                            content = text
+                                        )
+                                    }
+                                    .padding(16.dp),
+                                verticalArrangement = Arrangement.Center,
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                Text("Item $text")
+                            }
+                        }
+                    }
+                }
+            },
+            detailPane = {
+                val content = navigator.currentDestination?.content?.toString() ?: "No content"
+                AnimatedPane {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                    ) {
+                        Text("Detail: $content")
+                    }
+                }
+            }
+        )
+    }
 }
