fec4dd8a67fe3050d4bdd968ebdddf166b4333c8
aiyu-ayaan <aiyuayaan22@gmail.com>
2024-11-03 20:34:19 +0530

    feat: Add deeplink support for research path
    - Added a `researchPath` argument to the `OpenResearch` deep link route.
    - Updated the `MainScreen` route to accept the `researchPath` argument and handle navigation.
    - Added logging to display the `researchPath` when accessed via deep link.

--- composeApp/src/commonMain/kotlin/com/atech/research/ui/navigation/MainScreenNavGraph.kt
+++ composeApp/src/commonMain/kotlin/com/atech/research/ui/navigation/MainScreenNavGraph.kt
@@ -2,11 +2,15 @@ package com.atech.research.ui.navigation
 
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
 import androidx.navigation.navDeepLink
 import androidx.navigation.navigation
 import com.atech.research.ui.compose.main.MainScreen
 import com.atech.research.utils.DeepLink
+import com.atech.research.utils.ResearchLogLevel
 import com.atech.research.utils.fadeThroughComposable
+import com.atech.research.utils.researchHubLog
 
 sealed class MainScreenScreenRoutes(
     val route: String
@@ -20,14 +24,21 @@ fun NavGraphBuilder.mainScreenGraph(navController: NavController) {
         route = ResearchHubNavigation.MainScreen.route
     ) {
         fadeThroughComposable(
-            route = MainScreenScreenRoutes.HomeScreen.route,
-            deepLinks = listOf(
-                navDeepLink {
-                    uriPattern = DeepLink.OpenResearch().route
-                    action = "android.intent.action.VIEW"
-                }
-            )
+            route = MainScreenScreenRoutes.HomeScreen.route + "?researchPath={researchPath}",
+            arguments = listOf(navArgument("researchPath") {
+                nullable = true
+                defaultValue = null
+                type = NavType.StringType
+            }),
+            deepLinks = listOf(navDeepLink {
+                uriPattern = DeepLink.OpenResearch().route
+                action = "android.intent.action.VIEW"
+            })
         ) {
+            val researchPath = it.arguments?.getString("researchPath") ?: ""
+            if (researchPath.isNotBlank()) researchHubLog(
+                ResearchLogLevel.INFO, "ResearchPath: $researchPath"
+            )
             MainScreen(
                 navHostController = navController
             )
--- composeApp/src/commonMain/kotlin/com/atech/research/utils/Deeplink.kt
+++ composeApp/src/commonMain/kotlin/com/atech/research/utils/Deeplink.kt
@@ -4,8 +4,8 @@ package com.atech.research.utils
 const val BaseDeepLinkUrl = "https://aka-rhub.netlify.app/"
 
 sealed class DeepLink(val route: String) {
-    data class OpenResearch(private val researchPath: String="no_path") :
-        DeepLink("$BaseDeepLinkUrl/?researchPath=$researchPath")
+    data class OpenResearch(val researchPath: String = "{researchPath}") :
+        DeepLink("$BaseDeepLinkUrl/research/${researchPath}")
 
 }
 
