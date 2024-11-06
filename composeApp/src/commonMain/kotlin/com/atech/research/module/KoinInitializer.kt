4bf98ed9b2c72bf6774ce702a727c87cee34bba4
aiyu-ayaan <aiyuayaan22@gmail.com>
2024-11-06 20:25:12 +0530

    chore: Add documentation to KoinInitializer
    Added documentation to the `KoinInitializer` class, explaining its purpose as a wrapper for the koin initializer and noting that each platform will have its own implementation.

--- composeApp/src/commonMain/kotlin/com/atech/research/module/KoinInitializer.kt
+++ composeApp/src/commonMain/kotlin/com/atech/research/module/KoinInitializer.kt
@@ -1,5 +1,11 @@
 package com.atech.research.module
 
+/**
+ * Koin initializer
+ * This class is a wrapper for the koin initializer.
+ * Every platform will have its own implementation
+ * @constructor Create empty Koin initializer
+ */
 expect class KoinInitializer {
     fun init()
 }
